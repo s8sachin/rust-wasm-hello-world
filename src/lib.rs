d158d02bdede6a9b90696d86486b7c832b30e304
s8sachin <s.sachin94@gmail.com>
2019-06-23 04:06:55 +0530

    added Hello world example

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,19 @@ mod tests {
 pub extern fn add_one(x: u32) -> u32 {
     x + 1
 }
+
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+static HELLO: &'static str = "Hello from Rust";
+
+#[no_mangle]
+pub fn get_hello() -> *mut c_char {
+    let s = CString::new(HELLO).unwrap();
+    s.into_raw()
+}
+
+#[no_mangle]
+pub fn get_hello_len() -> usize {
+    HELLO.len()
+}
